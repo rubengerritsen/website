a9e5c4f932ddbba72d4ed6427b9a8a51a64006d1
rubengerritsen <r.h.j.gerritsen@gmail.com>
2020-07-22 15:10:51 +0200

    First Commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+export default function Home() {
+  return (
+    <div style={{ color: `purple` }}>
+      <Header headerText="Under Construction!" />
+    </div>
+  )
+}
