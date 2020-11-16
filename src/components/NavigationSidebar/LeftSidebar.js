8e83c1265d99a712f4d5a1433cd94b37a4af8485
rubengerritsen <r.h.j.gerritsen@gmail.com>
2020-11-16 21:51:29 +0100

    smallish update

--- src/components/NavigationSidebar/LeftSidebar.js
+++ src/components/NavigationSidebar/LeftSidebar.js
@@ -1,25 +1,20 @@
-import styled from '@emotion/styled';
-import React from 'react';
-import Navigation from './Navigation';
-
+import styled from "@emotion/styled"
+import React from "react"
+import Navigation from "./Navigation"
 
 const LeftSidebar = props => {
   const { url } = props
   return (
     <LeftSidebarWrapper>
-      <aside className="hiddenMobile" >
+      <aside className="hiddenMobile">
         <Navigation url={url} />
       </aside>
     </LeftSidebarWrapper>
-  );
-};
-
+  )
+}
 
 const LeftSidebarWrapper = styled.aside`
   overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  flex-shrink: 0;
   position: -webkit-sticky;
   position: -moz-sticky;
   position: sticky;
@@ -28,10 +23,9 @@ const LeftSidebarWrapper = styled.aside`
   font-size: 1.5rem;
   font-weight: 400;
   padding: 1rem;
-  max-width: 350px;
+  max-width: 280px;
   min-width: 150px;
-  height: 95vh;
-`;
+  height: 90vh;
+`
 
-
-export default LeftSidebar;
+export default LeftSidebar
--- src/components/RightSidebar.js
+++ src/components/RightSidebar.js
@@ -1,40 +1,41 @@
-import styled from '@emotion/styled';
-import React from 'react';
+import styled from "@emotion/styled"
+import React from "react"
 
 const RightSidebar = ({ tableOfContents }) => {
   return (
     <RightSidebarWrapper>
       <RightSidebarTitle>On this page</RightSidebarTitle>
       <RightSidebarList>
-        <div
-          dangerouslySetInnerHTML={{ __html: tableOfContents }}
-        />
+        <div dangerouslySetInnerHTML={{ __html: tableOfContents }} />
       </RightSidebarList>
     </RightSidebarWrapper>
-  );
-};
+  )
+}
 
 const RightSidebarWrapper = styled.aside`
   overflow: hidden;
-  position: fixed;
+  position: -webkit-sticky;
+  position: -moz-sticky;
+  position: sticky;
+  top: 3.2em;
+  display: block;
   font-size: 1.5rem;
   font-weight: 400;
   padding: 1rem;
-  max-width: 300px;
-  min-width: 250px;
-  max-height: 100vh;
-  border-style: none   none none solid;
+  max-width: 350px;
+  min-width: 200px;
+  max-height: 90vh;
+  border-style: none none none solid;
   border-width: 1px;
   border-color: rgba(172, 172, 172, 0.323);
-  
-`;
+`
 
 const RightSidebarTitle = styled.p`
   margin-top: 0.5em;
   margin-bottom: 0.5em;
   font-size: 1.5rem;
   font-weight: 400;
-`;
+`
 
 const RightSidebarList = styled.ul`
   display: block;
@@ -49,13 +50,13 @@ const RightSidebarList = styled.ul`
     margin: 0;
     padding: 0;
   }
-  & p{
+  & p {
     margin: 0;
     padding: 0;
     font-size: 1.3rem;
   }
   & li {
-    margin:  2px 2px 2px 8px ;
+    margin: 2px 2px 2px 8px;
     padding: 0em;
     font-size: 1.4rem;
   }
@@ -68,7 +69,6 @@ const RightSidebarList = styled.ul`
     font-size: 1.2rem;
     padding: 0em;
   }
-`;
+`
 
-
-export default RightSidebar;
+export default RightSidebar
